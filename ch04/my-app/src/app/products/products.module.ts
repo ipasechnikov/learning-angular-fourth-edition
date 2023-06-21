057ee72b79a89c299ddb38b0ec260a1e5e0510d1
Ilia Pasechnikov <ilia.pasechnikov@gmail.com>
2023-06-21 23:00:49 +0900

    Update AppComponent to display ProductListComponent

--- ch04/my-app/src/app/products/products.module.ts
+++ ch04/my-app/src/app/products/products.module.ts
@@ -13,6 +13,9 @@ import { ProductComponent } from './product/product.component';
   imports: [
     CommonModule,
     ProductComponent
+  ],
+  exports: [
+    ProductListComponent
   ]
 })
 export class ProductsModule { }
