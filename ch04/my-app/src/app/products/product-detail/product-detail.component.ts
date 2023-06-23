2f4e67d640fdfde9f0b138fb88ca503b004932e7
Ilia Pasechnikov <ilia.pasechnikov@gmail.com>
2023-06-24 01:22:49 +0900

    Use ChangeDetectionStrategy.OnPush for ProductDetailComponent

--- ch04/my-app/src/app/products/product-detail/product-detail.component.ts
+++ ch04/my-app/src/app/products/product-detail/product-detail.component.ts
@@ -1,14 +1,20 @@
-import { Component, EventEmitter, Input, Output, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css'],
-  encapsulation: ViewEncapsulation.Emulated
+  encapsulation: ViewEncapsulation.Emulated,
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ProductDetailComponent {
   @Input() name = '';
   @Output() bought = new EventEmitter<string>();
+
+  get productName(): string {
+    console.log(`Get ${this.name}`);
+    return this.name;
+  }
 
   buy(): void {
     this.bought.emit(this.name);
