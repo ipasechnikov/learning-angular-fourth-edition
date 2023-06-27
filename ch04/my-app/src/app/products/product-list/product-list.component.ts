4823500213095515f5900137239cc2923e2a05f9
Ilia Pasechnikov <ilia.pasechnikov@gmail.com>
2023-06-27 23:25:50 +0900

    Implement AfterViewInit interface for ProductListComponent

--- ch04/my-app/src/app/products/product-list/product-list.component.ts
+++ ch04/my-app/src/app/products/product-list/product-list.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { ProductDetailComponent } from '../product-detail/product-detail.component';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent {
+export class ProductListComponent implements AfterViewInit {
+  @ViewChild(ProductDetailComponent) productDetail?: ProductDetailComponent;
+
   selectedProduct = 'Microphone';
+
+  ngAfterViewInit(): void {
+    if (this.productDetail) {
+      console.log(this.productDetail.name);
+    }
+  }
 
   onBuy(name: string): void {
     window.alert(`You just bought ${name}`);
