c4402ca441d6e56429efeadb55ab4559a9e5c692
Ilia Pasechnikov <ilia.pasechnikov@gmail.com>
2023-07-03 00:27:00 +0900

    Create a standalone FilterPipe

--- ch05/my-app/src/app/app.module.ts
+++ ch05/my-app/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ProductsModule } from './products/products.module';
+import { FilterPipe } from './filter.pipe';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { ProductsModule } from './products/products.module';
   ],
   imports: [
     BrowserModule,
-    ProductsModule
+    ProductsModule,
+    FilterPipe
   ],
   providers: [],
   bootstrap: [AppComponent]
