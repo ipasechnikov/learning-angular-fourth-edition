ff15e4a412151f74bf7ceef08ddb76bef46924cf
Ilia Pasechnikov <ilia.pasechnikov@gmail.com>
2023-07-04 23:33:00 +0900

    Create CopyrightDirective

--- ch05/my-app/src/app/app.module.ts
+++ ch05/my-app/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ProductsModule } from './products/products.module';
 import { FilterPipe } from './filter.pipe';
+import { CopyrightDirective } from './copyright.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CopyrightDirective
   ],
   imports: [
     BrowserModule,
