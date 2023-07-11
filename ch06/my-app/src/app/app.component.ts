0981689f5e11ec8de705828bbcfafadd2caaa554
Ilia Pasechnikov <ilia.pasechnikov@gmail.com>
2023-07-12 00:52:07 +0900

    Add and inject AppConfig instance using InjectionToken

--- ch06/my-app/src/app/app.component.ts
+++ ch06/my-app/src/app/app.component.ts
@@ -1,13 +1,18 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component, OnDestroy, Inject } from '@angular/core';
+import { APP_CONFIG, AppConfig, appSettings } from './app.config';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [
+    { provide: APP_CONFIG, useValue: appSettings }
+  ]
 })
 export class AppComponent implements OnDestroy {
   title = 'my-app';
 
-  ngOnDestroy(): void {
-  }
+  constructor(@Inject(APP_CONFIG) config: AppConfig)  {}
+
+  ngOnDestroy(): void {}
 }
