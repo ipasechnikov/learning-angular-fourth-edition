3e014922e3c50c20a825e0cb88d36bb01e74983e
Ilia Pasechnikov <ilia.pasechnikov@gmail.com>
2023-07-13 00:49:40 +0900

    Implement title change using callbacks

--- ch07/my-app/src/app/app.component.ts
+++ ch07/my-app/src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'my-app';
+
+  constructor() {
+    this.changeTitle(this.setTitle);
+  }
+
+  private setTitle = () => {
+    this.title = 'Learning Angular';
+  };
+
+  private changeTitle(callback: Function) {
+    setTimeout(() => {
+      callback();
+    }, 2000);
+  }
+
 }
