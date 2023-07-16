eea5fd35e61f7c6462a531a694a7a36f5e6ccf1d
Ilia Pasechnikov <ilia.pasechnikov@gmail.com>
2023-07-16 23:48:59 +0900

    Update ProductViewComponent to unsubscribe from observable using Subject

--- ch07/my-app/src/app/products/product-view/product-view.component.ts
+++ ch07/my-app/src/app/products/product-view/product-view.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { ProductViewService } from './product-view.service';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-product-view',
@@ -7,11 +8,13 @@ import { ProductViewService } from './product-view.service';
   styleUrls: ['./product-view.component.css'],
   providers: [ProductViewService],
 })
-export class ProductViewComponent implements OnInit {
+export class ProductViewComponent implements OnInit, OnDestroy {
 
   name: string = '';
 
   @Input() id: number = -1;
+
+  private productSub = new Subject<void>();
 
   constructor(private readonly productViewService: ProductViewService) { }
 
@@ -19,8 +22,15 @@ export class ProductViewComponent implements OnInit {
     this.getProduct();
   }
 
+  ngOnDestroy(): void {
+    this.productSub.next();
+    this.productSub.complete();
+  }
+
   private getProduct(): void {
-    this.productViewService.getProduct(this.id).subscribe(product => {
+    this.productViewService.getProduct(this.id).pipe(
+      takeUntil(this.productSub)
+    ).subscribe(product => {
       this.name = product.name;
     });
   }
