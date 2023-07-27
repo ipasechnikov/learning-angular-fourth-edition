42fab8882a1567750087e33c22c75c37d977c951
Ilia Pasechnikov <ilia.pasechnikov@gmail.com>
2023-07-27 23:37:11 +0900

    Revert "Add router outlet to ProductList component"
    This reverts commit eab72cf1e87f1b25101d9e3d33796003395c902f.

--- ch09/my-app/src/app/products/products-routing.module.ts
+++ ch09/my-app/src/app/products/products-routing.module.ts
@@ -5,13 +5,8 @@ import { ProductDetailComponent } from './product-detail/product-detail.componen
 import { ProductListComponent } from './product-list/product-list.component';
 
 const routes: Routes = [
-  {
-    path: 'products',
-    component: ProductListComponent,
-    children: [
-      { path: ':id', component: ProductDetailComponent }
-    ]
-  },
+  { path: 'products', component: ProductListComponent },
+  { path: 'products/:id', component: ProductDetailComponent },
   { path: '', redirectTo: '/products', pathMatch: 'full' }
 ];
 
