de128a9e604f9659dfe1913772633c53bcef5765
Ilia Pasechnikov <ilia.pasechnikov@gmail.com>
2023-08-15 23:03:01 +0900

    Create Map component using Google Maps API

--- ch11/my-app/src/app/app.module.ts
+++ ch11/my-app/src/app/app.module.ts
@@ -1,16 +1,20 @@
-import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
-import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { GoogleMapsModule } from '@angular/google-maps';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
 import { CartComponent } from './cart/cart.component';
+import { MapComponent } from './map/map.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { ProductsModule } from './products/products.module';
 
@@ -18,7 +22,8 @@ import { ProductsModule } from './products/products.module';
   declarations: [
     AppComponent,
     CartComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule,
@@ -30,7 +35,8 @@ import { ProductsModule } from './products/products.module';
     CommonModule,
     BrowserAnimationsModule,
     MatButtonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    GoogleMapsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
